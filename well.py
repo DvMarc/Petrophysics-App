43783aa61cba49f113dd280a985815fcc36098c9
Rommel David Marcillo Pilay <romdmarc@espol.edu.ec>
2025-01-18 21:56:51 -0500

    seleccion de carpetas

--- well.py
+++ well.py
@@ -10,3 +10,14 @@ icon = Image.open('logo.jpg')
 st.set_page_config(page_title="Petrophysics App", page_icon=icon)
 
 st.title('Petrophysics App')
+
+st.write("""
+Esta aplicación permite analizar archivos de registros de pozos 
+en formato LAS del campo Volve, Noruega.
+""")
+
+data_path = Path('Data/VOLVE-PETROPHYSICAL_INTERPRETATION')
+folders = [folder for folder in data_path.iterdir() if folder.is_dir()]
+folder_names = [folder.name for folder in folders]
+
+folder = st.selectbox('Selecciona la carpeta', folder_names)
